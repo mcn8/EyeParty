ee9bbbe0d8ce206bad1b81883f68a1a5625e8d54
mccordnate <mccordnate@gmail.com>
2014-09-13 00:07:05 -0500

    Initial Commit

---
+++ EyeParty/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EyeParty
+//
+//  Created by Nate on 9/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ EyeParty/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  EyeParty
+//
+//  Created by Nate on 9/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
